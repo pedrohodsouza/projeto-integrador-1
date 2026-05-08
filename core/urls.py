dc3c7b6ec9df03afe29e9a67ef0edf2324536feb
Pedro de Souza <pedrohodesouza@gmail.com>
2026-05-08 17:01:53 -0300

    Cria os menus dos módulos e primeira versão do módulo de Registro

--- core/urls.py
+++ core/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
+    path('', include('denuncias.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ denuncias/forms.py
@@ -0,0 +1,61 @@
+from django import forms
+from .models import Denuncia
+
+class DenunciaForm(forms.ModelForm):
+    class Meta:
+        model = Denuncia
+        fields = ['titulo', 'categoria', 'descricao', 'cep', 'endereco', 'numero', 'bairro', 'cidade', 'estado', 'foto', 'anonimo']
+        widgets = {
+            'titulo': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Ex: Buraco profundo na pista principal'
+            }),
+            'categoria': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'descricao': forms.Textarea(attrs={
+                'class': 'form-textarea',
+                'placeholder': 'Descreva detalhadamente o problema urbano observando riscos...',
+                'rows': 4
+            }),
+            'cep': forms.TextInput(attrs={
+                'class': 'form-input',
+                'id': 'id_cep',
+                'placeholder': '00000-000',
+                'maxlength': '9'
+            }),
+            'endereco': forms.TextInput(attrs={
+                'class': 'form-input',
+                'id': 'id_endereco',
+                'placeholder': 'Rua, Avenida, etc.'
+            }),
+            'numero': forms.TextInput(attrs={
+                'class': 'form-input',
+                'id': 'id_numero',
+                'placeholder': 'Nº'
+            }),
+            'bairro': forms.TextInput(attrs={
+                'class': 'form-input',
+                'id': 'id_bairro',
+                'placeholder': 'Bairro'
+            }),
+            'cidade': forms.TextInput(attrs={
+                'class': 'form-input',
+                'id': 'id_cidade',
+                'placeholder': 'Cidade'
+            }),
+            'estado': forms.TextInput(attrs={
+                'class': 'form-input',
+                'id': 'id_estado',
+                'placeholder': 'UF',
+                'maxlength': '2'
+            }),
+            'foto': forms.FileInput(attrs={
+                'class': 'form-file-input',
+                'id': 'id_foto',
+                'accept': 'image/*'
+            }),
+            'anonimo': forms.CheckboxInput(attrs={
+                'class': 'form-checkbox'
+            })
+        }
