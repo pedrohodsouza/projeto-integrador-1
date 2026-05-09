5fd30e86a98e8c7b97002268aaf7522ebae5f6e3
Pedro <pedrohodesouza@gmail.com>
2026-05-08 21:36:40 -0300

    Acrescenta possibilidade de acompanhar chamados abertos no módulo de Registro

--- denuncias/urls.py
+++ denuncias/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('denunciar/', views.criar_denuncia, name='criar_denuncia'),
     path('denuncias/<int:pk>/sucesso/', views.sucesso_denuncia, name='sucesso_denuncia'),
+    path('acompanhar/', views.acompanhar_chamado, name='acompanhar_chamado'),
+    path('acompanhar/<int:pk>/', views.detalhe_chamado, name='detalhe_chamado'),
 ]
